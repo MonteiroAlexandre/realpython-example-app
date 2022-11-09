534504c9dd7db2a9afefc2a71ce4377f762a85d2
MonteiroAlexandre <monteiro.alexandreng@gmail.com>
2022-11-09 15:01:21 -0300

    Initialize Git repository

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    number = request.args.get("number", "")
+    if number:
+        double = doubling(number)
+    else:
+        double = ""
+
+    return (
+        """<form action="" method="get">
+                <input type="text" name="number">
+                <input type="submit" value="Apply">
+            </form>"""
+        + "Double: "
+        + double
+    )
+
+def doubling(number):
+    try:
+        double = int(number) * 2
+        return str(double)
+
+    except ValueError:
+        return "invalid input"
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
